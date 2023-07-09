a5e8c57379a15c9130b6a14b95546e78ce5577d0
Hino <kyokai1783@gmail.com>
2023-07-09 22:38:23 +0900

    botan

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,10 +3,19 @@ import { Inter } from 'next/font/google'
 import { Footer } from 'src/components/Footer'
 import { Main } from 'src/components/Main'
 import { Header } from 'src/Header'
+import { useCallback } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  },[])
+  
   return (
     <>
       <Head>
@@ -16,6 +25,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main title="Index Page" page="index" />
       <Footer />
     </>
