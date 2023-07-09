5d4bbb7aacdbc7ec5dbd88cbda007371507b296f
Hino <kyokai1783@gmail.com>
2023-07-10 03:14:00 +0900

    useCallbuck,useEfect

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,18 +10,18 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   const [foo, setFoo] = useState(1)
 
-  const handleClick=(e) => {
-    setFoo(foo => foo+1)
-  }
+  const handleClick=useCallback((e) => {
+    if(foo<10){
+      setFoo(foo => foo+1)
+    }
+  },[foo])
 
   useEffect(() => {
     document.body.style.backgroundColor ="purple";
     return()=>{
       document.body.style.backgroundColor ="";
     }
-  },[]);
-
-  console.log(foo);
+  },[foo]);
 
   return (
     <>
